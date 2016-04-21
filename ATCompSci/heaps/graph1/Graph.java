6b32f1b771d165d5ad3a3863aea8f63ed7a43b06
17sn0470 <17sn0470@HSS-101-12.westwindsor.k12.nj.us>
2016-04-21 10:48:32 -0400

    rlly pls help

--- ATCompSci/heaps/graph1/Graph.java
+++ ATCompSci/heaps/graph1/Graph.java
@@ -5,7 +5,8 @@ import java.util.TreeMap;
 
 public class Graph {
 	private TreeMap<String, HashSet<String>> map;
-
+	boolean match = false;
+	
 	public Graph(String line) {
 		map = new TreeMap<String, HashSet<String>>();
 		String[] a = line.split(" ");
@@ -36,22 +37,34 @@ public class Graph {
 		return map.containsKey(letter);
 	}
 
-	public boolean check(String first, String second) {
-		if(!map.containsKey(first))
-			return false;
-		if(map.get(first).contains(second))
-			return true;
-		
-		HashSet<String> set = map.get(first);
-		HashSet<String> solutions = map.get(first);
-		for(String s : set){
-			solutions.addAll(map.get(s));
+//	public boolean check(String first, String second) {
+//		if(!map.containsKey(first))
+//			return false;
+//		if(map.get(first).contains(second))
+//			return true;
+//		
+//		HashSet<String> set = map.get(first);
+//		HashSet<String> solutions = map.get(first);
+//		for(String s : set){
+//			solutions.addAll(map.get(s));
+//		}
+//		for(String s : set){
+//			solutions.addAll(map.get(s));
+//		}
+//
+//		return solutions.contains(second);
+//	}
+	
+	public void check(String one, String two, String been){
+		if(!match){
+			if(map.get(been).contains(two))
+				match = true;
+			else{
+				HashSet<String> set = map.get(been);
+				for(String x : set)
+					check(one, two, x);
+			}
 		}
-		for(String s : set){
-			solutions.addAll(map.get(s));
-		}
-
-		return solutions.contains(second);
 	}
 
 	public String toString() {
--- ATCompSci/heaps/graph1/GraphRunner.java
+++ ATCompSci/heaps/graph1/GraphRunner.java
@@ -16,13 +16,13 @@ public class GraphRunner
 	public static void main( String[] args ) throws IOException
 	{
 		Scanner file = new Scanner(new File("H:\\git\\ATCompSci\\ATCompSci\\heaps\\graph1\\graph1.dat"));
-		int howManyTimes = file.nextInt();
-		file.nextLine();
+		int howManyTimes = Integer.parseInt(file.nextLine());
 		for(int x=0; x<howManyTimes; x++)
 		{
-			Graph g = new Graph(file.nextLine());
+			Graph g = new Graph(" " + file.nextLine());
 			String check = file.nextLine();
-			System.out.println(g.check(check.substring(0, 1), check.substring(1)));
+			g.check(check.substring(0, 1), check.substring(1), check.substring(0, 1));
+			System.out.println(g.match);
 		}
 		file.close();
 	}
